6f344ac864a1ec1e973e71c6e29dbe1614267875
AniMB <animishmurthy@gmail.com>
2024-06-03 22:55:43 -0700

    Create read_serial.py

---
+++ testing/read_serial.py
@@ -0,0 +1,20 @@
+import serial
+
+def print():
+    try:
+        ser=serial.Serial(port="COM3", baudrate=9600)
+        if ser.in_waiting!=0:
+            print(ser.readline().decode())
+    except:
+        print(Exception)
+    finally:
+        ser.close()
+def send():
+    try:
+        ser=serial.Serial(port="COM3", baudrate=9600)
+        inp=input("Enter string to send")
+        ser.write(inp.encode())
+    except:
+        print(Exception)
+    finally:
+        ser.close()
