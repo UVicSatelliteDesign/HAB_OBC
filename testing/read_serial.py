47283d07ba839aa58129269c2a27dac31610f111
AniMB <animishmurthy@gmail.com>
2024-06-08 16:51:54 -0700

    Update read_serial.py

--- testing/read_serial.py
+++ testing/read_serial.py
@@ -1,8 +1,8 @@
 import serial
-
+ser=serial.Serial(port="COM3", baudrate=9600)
 def receive():
     try:
-        ser=serial.Serial(port="COM3", baudrate=9600)
+        
         if ser.in_waiting!=0:
             print(ser.readline().decode())
     except:
@@ -11,10 +11,12 @@ def receive():
         ser.close()
 def send():
     try:
-        ser=serial.Serial(port="COM3", baudrate=9600)
+        
         inp=input("Enter string to send")
         ser.write(inp.encode())
     except:
         print(Exception)
     finally:
         ser.close()
+while True:
+    print(ser.readline().decode())
